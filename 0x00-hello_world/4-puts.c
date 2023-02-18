30cb16aecb435423313121509adf511735103e9f
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-18 10:51:05 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-#include <stdio.h>
-
-int main() {
-    char str1[5];
-    strcpy(str1, "Programming is like building a multilingual puzzle);
-
-     puts(str1);
-
-     return(0);
+/
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+                return (0);
 }
