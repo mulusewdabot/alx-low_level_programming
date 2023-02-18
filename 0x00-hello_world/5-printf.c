fff808e4adb94128de5723a018e7905c135643c4
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-18 15:16:43 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
 * main - prints "with proper grammar, but the outcome is a piece of art,"
 * Return: Always 0 (success)
 */
 int main(void)
 {
-        printf("\"Programming is like building a multilingual puzzle");
-                return (0);
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 	
