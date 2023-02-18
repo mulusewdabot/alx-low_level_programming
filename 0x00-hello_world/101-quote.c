70a13c69d5278a070e4833646cd6984e672b8980
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-18 16:36:10 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-/*
-* main - prints "and that peice of art is useful" - dora korpar, 2015-10-19",
+/** main - prints "and that peice of art is useful" - dora korpar, 2015-10-19",
 * followed by a newline, to standard error.
 * Return: Always 1
 */
 
-int main()
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
