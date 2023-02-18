a4484024dcdb622d952a241f42a9614106f1de4d
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-18 11:18:28 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#!/bin/bash
-
-#include<studio.h>
- 
- int main()
+#include <stdio.h>
+/
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
 {
-	printF("with proper grammar, but the outcome is a piece of art,");
-
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+                return (0);
 }
+	
