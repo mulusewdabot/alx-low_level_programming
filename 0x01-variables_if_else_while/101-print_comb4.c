14e47067fcedbf9f112dfdd22a61e69ae20880b1
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-20 09:28:34 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,27 +9,27 @@
 
 int main(void)
 {
-        int a, b, c;
+int a, b, c;
 
-        for (a = '0'; a < '9'; a++)
-        {
-        for (b = a + 1; b <= '9'; b++)
-        {
-        for (c = b + 1; c <= '9'; c++)
-        {
-        if ((a != b) != c)
-        {
-        putchar(a);
-        putchar(b);
-        putchar(c);
-        if (a == '7' && b == '8')
-        continue;
-        putchar(',');
-        putchar(' ');
-        }
-        }
-        }
-        }
-        putchar('\n');
-        return (0);
+for (a = '0'; a < '9'; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+for (c = b + 1; c <= '9'; c++)
+{
+if ((a != b) != c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a == '7' && b == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
