fdab64d6388aade9be5d21630e4e16996839df40
MULUSEW DABOT <99834134+mulusewdabot@users.noreply.github.com>
2023-02-20 09:14:26 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,13 +6,13 @@
 
 int main(void)
 {
-        int n = 0;
+int n = 0;
 
-        while (n < 10)
-        {
-                printf("%i", n);
-                n++;
-        }
-        printf("\n");
-        return (0);
+while (n < 10)
+{
+printf("%i", n);
+n++;
+}
+printf("\n");
+return (0);
 }
