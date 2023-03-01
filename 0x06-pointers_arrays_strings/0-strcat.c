957103fa0d452c3428a8c710334564a9dc8b3887
mulusewdabot <mulusewdabot2007@gmail.com>
2023-03-01 00:05:15 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int k;
+	int n;
+
+	k = 0;
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[k] = src[n];
+		k++;
+		n++;
+	}
+
+	dest[k] = '\0';
+	return (dest);
+}
+
