1a3d4937e6058caf7f3ba7b76a1e3cf7bc08b4a6
mulusewdabot <mulusewdabot2007@gmail.com>
2023-03-20 12:10:26 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H
+#define DOG_H
 
 
 	/**
-	 * struct dog - structure of a dog
+	 * struct dog - a dog's basic info
 	 * @name: First member
 	 * @age: Second member
 	 * @owner: Third member
 	 *
 	 * Description: Longer description
 	 */
-	struct dog
+	struct dog()
 	{
 		char *name;
 		float age;
